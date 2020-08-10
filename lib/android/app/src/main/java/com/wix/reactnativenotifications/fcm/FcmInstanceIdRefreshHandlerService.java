53845cbd5cf344eb255d09233077f8a92fd804bb
Roberto Fierros Zepeda <robfz729@gmail.com>
2020-08-10 08:49:33 -0500

    Migrate IntentService to JobIntentService (based on migbot:job-intent-service)

--- lib/android/app/src/main/java/com/wix/reactnativenotifications/fcm/FcmInstanceIdRefreshHandlerService.java
+++ lib/android/app/src/main/java/com/wix/reactnativenotifications/fcm/FcmInstanceIdRefreshHandlerService.java
@@ -1,19 +1,25 @@
 package com.wix.reactnativenotifications.fcm;
 
-import android.app.IntentService;
+import android.support.annotation.NonNull;
+import android.support.v4.app.JobIntentService;
+import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
-public class FcmInstanceIdRefreshHandlerService extends IntentService {
+import static com.wix.reactnativenotifications.Defs.LOGTAG;
+
+public class FcmInstanceIdRefreshHandlerService extends JobIntentService {
 
     public static String EXTRA_IS_APP_INIT = "isAppInit";
     public static String EXTRA_MANUAL_REFRESH = "doManualRefresh";
 
-    public FcmInstanceIdRefreshHandlerService() {
-        super(FcmInstanceIdRefreshHandlerService.class.getSimpleName());
+    public static void enqueueWork(Context context, Intent work) {
+        enqueueWork(context, FcmInstanceIdRefreshHandlerService.class, JOB_ID, work);
     }
 
     @Override
-    protected void onHandleIntent(Intent intent) {
+    protected void onHandleWork(@NonNull Intent intent) {
+        Log.v(LOGCAT, 'Handling FCM token refresh');
         IFcmToken fcmToken = FcmToken.get(this);
         if (fcmToken == null) {
             return;
